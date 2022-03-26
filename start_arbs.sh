baf34dda9ee572b96fc58464a8b8a559eeaef8b5
Brian Long <bl@triton.one>
2022-03-26 16:14:36 -0600

    Change the active pairs

--- start_arbs.sh
+++ start_arbs.sh
@@ -1,13 +1,13 @@
-#node arb_spl.mjs MNGO USDC >> arb_spl.log &
-#node arb_spl.mjs SRM USDC >> arb_spl.log &
-#node arb_spl.mjs RAY USDC >> arb_spl.log &
-#node arb_spl.mjs ORCA USDC >> arb_spl.log &
-#node arb_spl.mjs STEP USDC >> arb_spl.log &
-#node arb_spl.mjs MER USDC >> arb_spl.log &
-#node arb_spl.mjs RIN USDC >> arb_spl.log &
-node arb_spl.mjs MSOL WSOL >> arb_spl.log &
-node arb_spl.mjs STSOL WSOL >> arb_spl.log &
-node arb_spl.mjs UST USDC >> arb_spl.log &
-node arb_spl.mjs USDT USDC >> arb_spl.log &
+node arb_spl.mjs MNGO USDC >> arb_spl.log &
+node arb_spl.mjs SRM USDC >> arb_spl.log &
+node arb_spl.mjs RAY USDC >> arb_spl.log &
+node arb_spl.mjs ORCA USDC >> arb_spl.log &
+node arb_spl.mjs STEP USDC >> arb_spl.log &
+node arb_spl.mjs MER USDC >> arb_spl.log &
+# node arb_spl.mjs RIN USDC >> arb_spl.log &
+# node arb_spl.mjs MSOL WSOL >> arb_spl.log &
+# node arb_spl.mjs STSOL WSOL >> arb_spl.log &
+# node arb_spl.mjs UST USDC >> arb_spl.log &
+# node arb_spl.mjs USDT USDC >> arb_spl.log &
 node arb_spl.mjs WSOL USDC >> arb_spl.log &
 ps ax | grep node
